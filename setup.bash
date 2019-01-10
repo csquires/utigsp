df5c807c54d7d0f08033807f5199a0bb236e6459
csquires <chandlersquires18@gmail.com>
2019-01-10 16:41:31 -0500

    change setup

--- setup.bash
+++ setup.bash
@@ -7,5 +7,5 @@ pip3 install -r requirements.txt
 python3 -m ipykernel install --user --name=off-target-igsp
 # ALL THIS CAN BE REMOVED AND CAUSALDAG CAN BE ADDED TO REQUIREMENTS AFTER IT IS PUBLISHED
 #pip3 install matplotlib typing
-yes | pip3 uninstall causaldag
-pip3 install --no-index --find-link file:/Users/chandlersquires/Documents/causaldag/dist/causaldag-0.1a42.tar.gz causaldag
+#yes | pip3 uninstall causaldag
+#pip3 install --no-index --find-link file:/Users/chandlersquires/Documents/causaldag/dist/causaldag-0.1a42.tar.gz causaldag
