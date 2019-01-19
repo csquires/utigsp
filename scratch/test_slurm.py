7ab4833909c19e043b1f9d82a7db9d2af76c00e0
csquires <chandlersquires18@gmail.com>
2019-01-19 12:49:02 -0500

    change test script

--- scratch/test_slurm.py
+++ scratch/test_slurm.py
@@ -1,7 +1,8 @@
 import argparse
+import numpy as np
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--test')
+parser.add_argument('--test', type=float)
 args = parser.parse_args()
-print(args.test)
+np.savetxt('%s.txt' % args.test, np.array([args.test]))
 
