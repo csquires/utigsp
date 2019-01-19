7586ffbd3de376a20aadd2f40fde23db7e5a26f6
csquires <chandlersquires18@gmail.com>
2019-01-19 12:37:52 -0500

    replace bad subbing

--- scratch/test_slurm.sh
+++ scratch/test_slurm.sh
@@ -2,7 +2,7 @@ for alpha in 0.0001 0.001 0.01 0.1
 do
     file=`test_file`
     echo "python3 -m test_slurm.py --test ${alpha}" > tmp.sh
-    cat slurm_template.sh tmp.sh > ${file.sh}
+    cat slurm_template.sh tmp.sh > ${file}.sh
     rm tmp.sh
     sbatch ${file}.sh
 done
