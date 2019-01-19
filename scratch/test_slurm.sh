a03c893f6451850c55978049202a939202741f1a
csquires <chandlersquires18@gmail.com>
2019-01-19 12:41:03 -0500

    correct filename

--- scratch/test_slurm.sh
+++ scratch/test_slurm.sh
@@ -1,6 +1,7 @@
 for alpha in 0.0001 0.001 0.01 0.1
 do
-    file=`test_file`
+    file='test_file'
+    echo ${file}
     echo "python3 -m test_slurm.py --test ${alpha}" > tmp.sh
     cat slurm_template.sh tmp.sh > ${file}.sh
     rm tmp.sh
