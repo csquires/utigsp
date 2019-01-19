70fdbbf80776f92b4d4113f5dc184f436e542a2a
csquires <chandlersquires18@gmail.com>
2019-01-19 12:46:45 -0500

    change partition

--- scratch/slurm_template.sh
+++ scratch/slurm_template.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-#SBATCH -N 32
-#SBATCH -n 32
+#SBATCH --ntasks 1
+#SBATCH --cpus-per-task 32
 #SBATCH --time 0:00:10
-#SBATCH -p sched_mit_nse
+#SBATCH -p newnodes
+#SBATCH -J test
 
 module add engaging/python/3.6.0
 source ../venv/bin/activate
