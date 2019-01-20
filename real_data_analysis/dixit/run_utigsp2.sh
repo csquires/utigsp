ba4290f8bb5775cab2b04b107e7ad7822845968a
csquires <chandlersquires18@gmail.com>
2019-01-19 23:49:47 -0500

    change script not to use -m

--- real_data_analysis/dixit/run_utigsp2.sh
+++ real_data_analysis/dixit/run_utigsp2.sh
@@ -5,7 +5,7 @@ do
     for alpha in 0.0001 0.001 0.01 0.1
     do
         file='test_file'
-        echo "python3 -m dixit_run_utigsp2.py --alpha ${alpha} --ci_test ${ci_test}" > tmp.sh
+        echo "python3 dixit_run_utigsp2.py --alpha ${alpha} --ci_test ${ci_test}" > tmp.sh
         cat slurm_template.sh tmp.sh > ${file}.sh
         rm tmp.sh
         sbatch ${file}.sh
