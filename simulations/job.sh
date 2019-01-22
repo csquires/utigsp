465e885c62283a62be84e7913800a2e9db9e8bb1
csquires <csquires@eofe7.cm.cluster>
2019-01-21 23:15:24 -0500

    add dags

--- simulations/job.sh
+++ simulations/job.sh
@@ -2,10 +2,10 @@
 
 #SBATCH --ntasks 1
 #SBATCH --cpus-per-task 32
-#SBATCH --time 2:00:00
-#SBATCH -p newnodes 
+#SBATCH --time 4:00:00
+#SBATCH -p sched_engaging_default
 #SBATCH -J simulations
 
 module add engaging/python/3.6.0
 source ../venv/bin/activate
-python3 run_utigsp.py --nnodes 10 --nneighbors 1.5 --ndags 50 --nsamples 500 --nsettings 5 --num_known 1 --num_unknown 3 --intervention perfect1 --alpha 1e-05 --alpha_invariant 1e-05
+python3 run_utigsp.py --nnodes 20 --nneighbors 1.5 --ndags 100 --nsamples 1000 --nsettings 3 --num_known 1 --num_unknown 3 --intervention perfect1 --alpha 1e-05 --alpha_invariant 1e-05
