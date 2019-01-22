d950e616b0076ca8c4c3d0d19740e435fb18d83f
csquires <chandlersquires18@gmail.com>
2019-01-21 22:49:30 -0500

    use engaging default

--- simulations/slurm_template.sh
+++ simulations/slurm_template.sh
@@ -3,7 +3,7 @@
 #SBATCH --ntasks 1
 #SBATCH --cpus-per-task 32
 #SBATCH --time 4:00:00
-#SBATCH -p newnodes 
+#SBATCH -p sched_engaging_default
 #SBATCH -J simulations
 
 module add engaging/python/3.6.0
