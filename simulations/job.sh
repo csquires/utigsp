9d2187bcc344f750f1a3c244c3766be1f77c4c29
csquires <csquires@eofe7.cm.cluster>
2019-01-22 07:31:27 -0500

    8 node results

--- simulations/job.sh
+++ simulations/job.sh
@@ -2,10 +2,10 @@
 
 #SBATCH --ntasks 1
 #SBATCH --cpus-per-task 32
-#SBATCH --time 0:10:00
+#SBATCH --time 12:00:00
 #SBATCH -p newnodes
 #SBATCH -J simulations
 
 module add engaging/python/3.6.0
 source ../venv/bin/activate
-python3 run_utigsp.py --nnodes 10 --nneighbors 1.5 --ndags 50 --nsamples 500 --nsettings 5 --num_known 1 --num_unknown 3 --intervention perfect1 --alpha 1e-05 --alpha_invariant 1e-05
+python3 run_igsp.py --nnodes 20 --nneighbors 1.5 --ndags 100 --nsamples 500 --nsettings 3 --num_known 1 --num_unknown 3 --intervention perfect1 --alpha 1e-05 --alpha_invariant 1e-05
