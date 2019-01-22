49352ddcf0256180004f3646f2f98530b683ed49
csquires <csquires@eofe7.cm.cluster>
2019-01-21 20:33:08 -0500

    git other results

--- simulations/job.sh
+++ simulations/job.sh
@@ -8,4 +8,4 @@
 
 module add engaging/python/3.6.0
 source ../venv/bin/activate
-python3 run_igsp.py --nnodes 10 --nneighbors 1.5 --ndags 50 --nsamples 500 --nsettings 5 --num_known 1 --num_unknown 3 --intervention perfect1 --alpha 1e-05 --alpha_invariant 1e-05
+python3 run_utigsp.py --nnodes 10 --nneighbors 1.5 --ndags 50 --nsamples 500 --nsettings 5 --num_known 1 --num_unknown 3 --intervention perfect1 --alpha 1e-05 --alpha_invariant 1e-05
