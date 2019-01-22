e5f0b1a1d06c9113cd7e6c1d2d4168882c36a438
csquires <chandlersquires18@gmail.com>
2019-01-22 18:38:57 -0500

    upgrade slurm template

--- simulations/slurm_template.sh
+++ simulations/slurm_template.sh
@@ -3,8 +3,8 @@
 #SBATCH --ntasks 1
 #SBATCH --cpus-per-task 32
 #SBATCH --time 06:00:00
-#SBATCH -p sched_engaging_default
-#SBATCH -J get_adds_dels
+#SBATCH -p newnodes
+#SBATCH -J igsp
 
 module add engaging/python/3.6.0
 source ../venv/bin/activate
