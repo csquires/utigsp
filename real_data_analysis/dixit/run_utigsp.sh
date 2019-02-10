4f24bbdbd70f7ac2f539500a641c8139f25e1bc9
csquires <chandlersquires18@gmail.com>
2019-02-10 11:57:00 -0500

    gies results

--- real_data_analysis/dixit/run_utigsp.sh
+++ real_data_analysis/dixit/run_utigsp.sh
@@ -4,7 +4,7 @@ for ci_test in gauss_ci hsic
 do
     for exclude in 2 9 15 16 17 20 21 22
     do
-        for alpha in 0.0001 0.001 0.01 0.1
+        for alpha in 0.1 0.2 0.3 0.4 0.5 0.6 0.7
         do
             file='test_file'
             echo "python3 -m dixit_run_utigsp.py --alpha ${alpha} --exclude ${exclude} --ci_test ${ci_test}" > tmp.sh
--- real_data_analysis/dixit/slurm_template.sh
+++ real_data_analysis/dixit/slurm_template.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #SBATCH --ntasks 1
-#SBATCH --cpus-per-task 32
+#SBATCH --cpus-per-task 16
 #SBATCH --time 12:00:00
 #SBATCH -p sched_engaging_default
 #SBATCH -J dixit-analysis
