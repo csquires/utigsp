7c0ff1fbfc25be729733e9a2a49b1dec38e130ba
csquires <chandlersquires18@gmail.com>
2019-02-20 15:49:52 -0500

    use multiprocessing

--- real_data_analysis/dixit/dixit_run_utigsp.py
+++ real_data_analysis/dixit/dixit_run_utigsp.py
@@ -6,55 +6,66 @@ from causaldag.inference.structural import unknown_target_igsp
 from causaldag.utils.ci_tests import gauss_ci_test, hsic_invariance_test, hsic_test
 from real_data_analysis.dixit.dixit_meta import get_sample_dict, ESTIMATED_FOLDER, nnodes
 import numpy as np
+import multiprocessing
 
-# === PARSE
-parser = argparse.ArgumentParser()
-parser.add_argument('--alpha', type=float)
-parser.add_argument('--excluded', type=int)
-parser.add_argument('--ci_test', type=str)
-args = parser.parse_args()
-alpha = args.alpha
-excluded = args.excluded
-ci_test = args.ci_test
 
-# === LOAD SAMPLES AND REMOVE EXCLUDED
-sample_dict, suffstat = get_sample_dict()
-print([v.shape[0] for k, v in sample_dict.items()])
-print(sum(v.shape[0] for k, v in sample_dict.items()))
-sample_dict_exclude = {k: v for k, v in sample_dict.items() if k != frozenset({excluded})}
-setting_list = [{'known_interventions': iv_nodes, 'samples': samples} for iv_nodes, samples in sample_dict_exclude.items()]
+if __name__ == '__main__':
+    # === PARSE
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--alpha', type=float)
+    parser.add_argument('--ci_test', type=str)
+    args = parser.parse_args()
+    alpha = args.alpha
+    ci_test = args.ci_test
 
-# === CREATE FILENAME
-alpha_invariance = 1e-5
-iv_estimated_folder = os.path.join(ESTIMATED_FOLDER, 'exclude_%s' % excluded)
-filename = os.path.join(iv_estimated_folder,
-                        'utigsp_%s_alpha=%.2e,alpha_i=%.2e.txt' % (ci_test, alpha, alpha_invariance))
+    # === LOAD SAMPLES AND REMOVE EXCLUDED
+    sample_dict, suffstat = get_sample_dict()
+    print([v.shape[0] for k, v in sample_dict.items()])
+    print(sum(v.shape[0] for k, v in sample_dict.items()))
+    setting_list = [{'known_interventions': iv_nodes, 'samples': samples} for iv_nodes, samples in sample_dict.items()]
 
-# === RUN ALGORITHM
-if not os.path.exists(filename):
-    if ci_test == 'gauss_ci':
-        est_dag = unknown_target_igsp(
-            sample_dict[frozenset()],
-            setting_list,
-            suffstat,
-            nnodes,
-            gauss_ci_test,
-            hsic_invariance_test,
-            alpha=alpha,
-            nruns=10,
-            alpha_invariance=alpha_invariance
-        )
-    else:
-        est_dag = unknown_target_igsp(
-            sample_dict[frozenset()],
-            setting_list,
-            sample_dict_exclude[frozenset()],
-            nnodes,
-            hsic_test,
-            hsic_invariance_test,
-            alpha=alpha,
-            nruns=10,
-            alpha_invariance=alpha_invariance
-        )
-    np.savetxt(filename, est_dag.to_amat())
+    obs_samples = sample_dict[frozenset()]
+    alpha_invariance = 1e-5
+
+    # === RUN ALGORITHM
+    def _run_alg(excluded):
+        setting_list_exclude = [setting for setting in setting_list if excluded not in setting['known_interventions']]
+        iv_estimated_folder = os.path.join(ESTIMATED_FOLDER, 'exclude_%s' % excluded)
+        filename = os.path.join(iv_estimated_folder,
+                                'utigsp_effective_%s_alpha=%.2e,alpha_i=%.2e.txt' % (ci_test, alpha, alpha_invariance))
+        if not os.path.exists(filename):
+            if ci_test == 'gauss_ci':
+                os.system('touch gauss_ci_utigsp_%d.tst' % excluded)
+                est_dag = unknown_target_igsp(
+                    obs_samples,
+                    setting_list_exclude,
+                    suffstat,
+                    nnodes,
+                    gauss_ci_test,
+                    hsic_invariance_test,
+                    alpha=alpha,
+                    nruns=10,
+                    alpha_invariance=alpha_invariance,
+                )
+            elif ci_test == 'hsic':
+                os.system('touch hsic_utigsp_%d.tst' % excluded)
+                est_dag = unknown_target_igsp(
+                    obs_samples,
+                    setting_list_exclude,
+                    obs_samples,
+                    nnodes,
+                    hsic_test,
+                    hsic_invariance_test,
+                    alpha=alpha,
+                    nruns=10,
+                    alpha_invariance=alpha_invariance,
+                )
+            else:
+                raise ValueError
+            np.savetxt(filename, est_dag.to_amat())
+
+
+    with multiprocessing.Pool(multiprocessing.cpu_count() - 1) as pool:
+        nodes = list(range(24))
+        list(pool.imap_unordered(_run_alg, nodes))
 
--- real_data_analysis/dixit/run_utigsp.sh
+++ real_data_analysis/dixit/run_utigsp.sh
@@ -2,15 +2,12 @@
 
 for ci_test in gauss_ci hsic
 do
-    for exclude in 2 9 15 16 17 20 21 22
+    for alpha in .001 .005 .01 .05 .1 .2 .3 .4 .5
     do
-        for alpha in 0.1 0.2 0.3 0.4 0.5 0.6 0.7
-        do
-            file='test_file'
-            echo "python3 -m dixit_run_utigsp.py --alpha ${alpha} --exclude ${exclude} --ci_test ${ci_test}" > tmp.sh
-            cat slurm_template.sh tmp.sh > ${file}.sh
-            rm tmp.sh
-            sbatch ${file}.sh
-        done
+        file='test_file'
+        echo "python3 -m dixit_run_utigsp.py --alpha ${alpha} --ci_test ${ci_test}" > tmp.sh
+        cat slurm_template.sh tmp.sh > ${file}.sh
+        rm tmp.sh
+        sbatch ${file}.sh
     done
 done
