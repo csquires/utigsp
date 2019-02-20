3617688fc291fbbcb422ba8826e77ee6c68f7f14
csquires <chandlersquires18@gmail.com>
2019-02-20 15:59:01 -0500

    remove -m

--- real_data_analysis/dixit/run_igsp.sh
+++ real_data_analysis/dixit/run_igsp.sh
@@ -5,7 +5,7 @@ do
     for alpha in .001 .005 .01 .05 .1 .2 .3 .4 .5
     do
         file='test_file'
-        echo "python3 -m dixit_run_igsp.py --alpha ${alpha} --ci_test ${ci_test}" > tmp.sh
+        echo "python3 dixit_run_igsp.py --alpha ${alpha} --ci_test ${ci_test}" > tmp.sh
         cat slurm_template.sh tmp.sh > ${file}.sh
         rm tmp.sh
         sbatch ${file}.sh
