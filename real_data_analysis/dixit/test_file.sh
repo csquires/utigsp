91431dd228c945380b2fe5f0f7bc18065d074b2a
csquires <csquires@eofe7.cm.cluster>
2019-02-28 11:41:51 -0500

    utigsp results

--- real_data_analysis/dixit/test_file.sh
+++ real_data_analysis/dixit/test_file.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 #SBATCH --ntasks 1
-#SBATCH --cpus-per-task 32
+#SBATCH --cpus-per-task 16
 #SBATCH --time 12:00:00
 #SBATCH -p sched_engaging_default
 #SBATCH -J dixit-analysis
 
 module add engaging/python/3.6.0
 source ../../venv/bin/activate
-python3 dixit_run_utigsp2.py --alpha .7 --ci_test hsic
+python3 dixit_run_utigsp.py --alpha .5 --ci_test hsic
