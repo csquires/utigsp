c9c42dc4750cddffe11f261906b9f1ba6a472ea3
csquires <csquires@eofe7.cm.cluster>
2019-03-12 18:04:25 -0400

    20 node results

--- simulations/job.sh
+++ simulations/job.sh
@@ -8,4 +8,4 @@
 
 module add engaging/python/3.6.0
 source ../venv/bin/activate
-python3 run_utigsp.py --nnodes 40 --nneighbors 1.5 --ndags 100 --nsamples 500 --nsettings 3 --num_known 1 --num_unknown 3 --intervention perfect1 --alpha 1e-05 --alpha_invariant 1e-05
+python3 run_utigsp.py --nnodes 20 --nneighbors 5 --ndags 100 --nsamples 500 --nsettings 2 --num_known 1 --num_unknown 3 --intervention perfect1 --alpha 1e-05 --alpha_invariant 1e-05 --pooled false
