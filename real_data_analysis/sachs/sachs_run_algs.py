ec5e1a6e51907d35c1fe22b7684b3c4ee19c9530
Chandler <csquires@mit.edu>
2020-04-18 12:49:01 -0400

    update utigsp on real data

--- real_data_analysis/sachs/sachs_run_algs.py
+++ real_data_analysis/sachs/sachs_run_algs.py
@@ -2,9 +2,10 @@ import numpy as np
 import pandas as pd
 from config import PROJECT_FOLDER
 import os
-from causaldag.inference.structural import unknown_target_igsp, igsp
+from causaldag import unknown_target_igsp, igsp
 from R_algs.wrappers import run_gies, run_icp
-from causaldag.utils.ci_tests import hsic_invariance_test, gauss_ci_test, hsic_test
+from causaldag.utils.ci_tests import gauss_ci_test, hsic_test, MemoizedCI_Tester, gauss_ci_suffstat
+from causaldag.utils.invariance_tests import hsic_invariance_test, MemoizedInvarianceTester, gauss_invariance_test, gauss_invariance_suffstat
 from real_data_analysis.sachs.sachs_meta import nnodes, SACHS_DATA_FOLDER, ESTIMATED_FOLDER
 import shutil
 from tqdm import tqdm
@@ -20,54 +21,52 @@ for file in os.listdir(SACHS_DATA_FOLDER):
     sample_dict[ivs] = samples.values
 obs_samples = sample_dict[frozenset()]
 all_samples = np.concatenate(tuple(sample_dict.values()), axis=0)
-suffstat = dict(C=np.corrcoef(obs_samples, rowvar=False), n=obs_samples.shape[0])
+suffstat = gauss_ci_suffstat(obs_samples)
 suffstat_all = dict(C=np.corrcoef(all_samples, rowvar=False), n=all_samples.shape[0])
 
 setting_list = [
-    {'known_interventions': iv_nodes, 'samples': samples}
+    {'known_interventions': iv_nodes}
     for iv_nodes, samples in sample_dict.items()
     if iv_nodes != frozenset()
 ]
+iv_samples_list = [sample_dict[setting['known_interventions']] for setting in setting_list]
+invariance_suffstat = gauss_invariance_suffstat(obs_samples, iv_samples_list)
 
-# # === RUN UNKNOWN TARGET IGSP WITH GAUSS CI
-# for alpha in [6e-1, 7e-1]:
-#     alpha_i = 1e-5
-#     filename = os.path.join(ESTIMATED_FOLDER, 'utigsp_gauss_ci_alpha=%.2e.txt,alpha_i=%.2e.txt' % (alpha, alpha_i))
-#     if not os.path.exists(filename):
-#         est_dag, _ = unknown_target_igsp(
-#             sample_dict[frozenset()],
-#             setting_list,
-#             suffstat,
-#             nnodes,
-#             gauss_ci_test,
-#             hsic_invariance_test,
-#             alpha=alpha,
-#             nruns=10,
-#             alpha_invariance=1e-5
-#         )
-#         np.savetxt(filename, est_dag.to_amat())
+# === RUN UNKNOWN TARGET IGSP WITH GAUSS CI
+for alpha in [6e-1, 7e-1]:
+    alpha_i = 1e-5
+    filename = os.path.join(ESTIMATED_FOLDER, 'utigsp_gauss_ci_alpha=%.2e.txt,alpha_i=%.2e.txt' % (alpha, alpha_i))
+    ci_tester = MemoizedCI_Tester(gauss_ci_test, suffstat, alpha=alpha)
+    invariance_tester = MemoizedInvarianceTester(gauss_invariance_test, invariance_suffstat, alpha=alpha_i)
+    if not os.path.exists(filename):
+        est_dag, _ = unknown_target_igsp(
+            setting_list,
+            set(range(nnodes)),
+            ci_tester,
+            invariance_tester,
+            nruns=10,
+        )
+        np.savetxt(filename, est_dag.to_amat())
 
-# # === RUN UNKNOWN TARGET IGSP WITH GAUSS CI AND TARGETS REMOVED
-# alpha = 3e-1
-# for alpha_i in tqdm([1e-100, 1e-50]):
-#     file = 'utigsp_gauss_ci_unknown_alpha=%.2e,alpha_i=%.2e.txt' % (alpha, alpha_i)
-#     filename = os.path.join(ESTIMATED_FOLDER, file)
-#     setting_list_removed = [{'known_interventions': [], 'samples': setting['samples']} for setting in setting_list]
-#     if not os.path.exists(filename):
-#         est_dag, learned_interventions = unknown_target_igsp(
-#             sample_dict[frozenset()],
-#             setting_list_removed,
-#             suffstat,
-#             nnodes,
-#             gauss_ci_test,
-#             hsic_invariance_test,
-#             alpha=alpha,
-#             nruns=10,
-#             alpha_invariance=1e-5
-#         )
-#         print(learned_interventions)
-#         json.dump(list(map(list, learned_interventions)), open(os.path.join(ESTIMATED_FOLDER, 'learned_interventions_' + file), 'w'))
-#         np.savetxt(filename, est_dag.to_amat())
+# === RUN UNKNOWN TARGET IGSP WITH GAUSS CI AND TARGETS REMOVED
+alpha = 3e-1
+for alpha_i in tqdm([1e-100, 1e-50]):
+    file = 'utigsp_gauss_ci_unknown_alpha=%.2e,alpha_i=%.2e.txt' % (alpha, alpha_i)
+    filename = os.path.join(ESTIMATED_FOLDER, file)
+    ci_tester = MemoizedCI_Tester(gauss_ci_test, suffstat, alpha=alpha)
+    invariance_tester = MemoizedInvarianceTester(gauss_invariance_test, invariance_suffstat, alpha=alpha_i)
+    setting_list_removed = [{'known_interventions': []} for setting in setting_list]
+    if not os.path.exists(filename):
+        est_dag, learned_interventions = unknown_target_igsp(
+            setting_list_removed,
+            set(range(nnodes)),
+            ci_tester,
+            invariance_tester,
+            nruns=10,
+        )
+        print(learned_interventions)
+        json.dump(list(map(list, learned_interventions)), open(os.path.join(ESTIMATED_FOLDER, 'learned_interventions_' + file), 'w'))
+        np.savetxt(filename, est_dag.to_amat())
 
 # # === RUN UNKNOWN TARGET IGSP WITH HSIC
 # for alpha in tqdm([4e-1]):
